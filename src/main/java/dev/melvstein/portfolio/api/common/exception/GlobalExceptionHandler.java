6f87f9f9fd836f2705d1bf917d86380e351dcab8
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2026-06-22 22:52:07 +0800

    melvstein portfolio api

--- src/main/java/dev/melvstein/portfolio/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/melvstein/portfolio/api/common/exception/GlobalExceptionHandler.java
@@ -1,19 +1,13 @@
 package dev.melvstein.portfolio.api.common.exception;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import dev.melvstein.portfolio.api.base.vo.BaseResponseVo;
 import dev.melvstein.portfolio.api.base.vo.ErrorResponseVo;
-import dev.melvstein.portfolio.api.user.enm.RoleEnum;
-import dev.melvstein.portfolio.api.user.enm.StatusEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
